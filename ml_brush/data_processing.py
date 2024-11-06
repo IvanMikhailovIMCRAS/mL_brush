8958829e1189c752348be9c41971af95782ea6fd
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2024-11-06 13:47:31 +0300

    added new parameters

--- ml_brush/data_processing.py
+++ ml_brush/data_processing.py
@@ -4,25 +4,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    # data = np.loadtxt("file.txt").T
-    # columns = {
-    #     "n": data[0],
-    #     "m": data[1],
-    #     "p": data[2],
-    #     "X": data[3],
-    #     "chi": data[4],
-    #     "phi": data[5]
-    # }
-    # df = pd.DataFrame(columns)
-    # df.to_csv('comb_brush_mean_phi.csv', encoding='utf-8', index=False, header=True)
+    data = np.loadtxt("file.txt").T
+    X = data[3]
+    chi = data[4]
+    new_chi = 1 / (2 + X) * (np.tanh(chi-0.5) + 1 + X)
+    rev_eta = (1 + data[0] / data[1])**(-1)
+    columns = {
+        "rev_eta": rev_eta,
+        "rev_p": 1 / data[2],
+        "X": data[3],
+        "new_chi": new_chi,
+        "phi": data[5]        
+    }
+    df = pd.DataFrame(columns)
+    df.to_csv('comb_brush_mean_phi.csv', encoding='utf-8', index=False, header=True)
     df = pd.read_csv("comb_brush_mean_phi.csv") 
-    # print(df.head(10))
-    # sns.histplot(df[['X','chi']], kde=True)
-    # plt.show()
+    print(df.head(10))
+    sns.histplot(df[['rev_eta','new_chi']], kde=True)
+    plt.show()
     print(df[['X','phi']].describe())
     print(df[df['phi']>0.99])
     
-    corr = df[['n','m', 'p', 'X', 'chi', 'phi']].corr()
-    print(corr)
+    # corr = df[['rev_eta', 'rev_p', 'X', 'new_chi', 'phi']].corr(method="spearman")
+    # print(corr)
     # sns.heatmap(corr, cmap="crest")
     # plt.show()
